c4cb7941569e62ee3b044139494bcbe04a1bb686
romay <i_romay@naver.com>
2024-01-20 15:19:04 +0900

    리액트 기초강좌 수강 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let [글제목, 글제목변경] = useState(["애월", "제주", "제주공항"]);
+  let [따봉, 따봉변경] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">MovieProject</div>
+      <div className="list">
+        <h4 className="">
+          {글제목[0]}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}>
+            😍
+          </span>
+          {따봉}
+        </h4>
+        <p>날짜</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h4 className="">{글제목[1]}</h4>
+        <p>날짜</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h4 className="">{글제목[2]}</h4>
+        <p>날짜</p>
+        <hr />
+      </div>
+      <Modal></Modal>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h3>글 제목</h3>
+      <p>글 내용</p>
     </div>
   );
 }
